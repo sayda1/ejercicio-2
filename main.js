7a0ef8ffd08385ebff35ec621c19144a451477ee
sayda1 <saydaquintanilla1@gmail.com>
2016-12-20 10:32:00 -0500

    promedio

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var numero1, numero2, numero3 , promedio, suma;
-numero1= prompt("Ingrese el primer nùmero")
-numero2= prompt("Ingrese el segundo nùmero")
-numero3= prompt("Ingrese el tercer nùmero")
+numero1= prompt("Ingrese el primer nùmero");
+numero2= prompt("Ingrese el segundo nùmero");
+numero3= prompt("Ingrese el tercer nùmero");
 
 suma= parseInt(numero1) + parseInt(numero2) + parseInt(numero3);
 
 promedio = suma/3;
 
-alert(promedio);
+alert (promedio);
