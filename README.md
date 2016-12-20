b330045f494ae52bd8d080000f3894245eff1faf
sayda1 <saydaquintanilla1@gmail.com>
2016-12-19 20:17:07 -0500

    codigos

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ejercicio-2
+Inicio:sacar promedio
+
+## Definir variables
+- numero1
+- numero2
+- numero3
+- promedio
+- suma
+## Proceso
+- Pedir tres numeros
+- Sumar los tres numeros ingresados
+- dividir la suma entre tres
+- mostrar resultado
+
+*fin*
---
+++ main.js
@@ -0,0 +1,10 @@
+var numero1, numero2, numero3 , promedio, suma;
+numero1= prompt("Ingrese el primer nùmero")
+numero2= prompt("Ingrese el segundo nùmero")
+numero3= prompt("Ingrese el tercer nùmero")
+
+suma= parseInt(numero1) + parseInt(numero2) + parseInt(numero3);
+
+promedio = suma/3;
+
+alert(promedio);
